1d3f6f7cc10fdba7763b982ebc210e30e41f126c
Mohamed Adel <Mohamed Adel>
2022-05-22 22:10:59 +0200

    inital commit

---
+++ BetterBuy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BetterBuy
+//
+//  Created by Mohamed Adel on 5/22/22.
+//  Copyright © 2022 Mohamed Adel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
